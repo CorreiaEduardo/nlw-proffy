540f6327de6de83d226ce02066e7f59b1a411411
Eduardo Correia <correiaeduardojr@gmail.com>
2020-08-09 00:01:45 -0300

    ✨ add class five features with minor changes

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { View, Image, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
@@ -10,10 +10,20 @@ import studyIcon from '../../assets/images/icons/study.png';
 import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
 
+import api from '../../services/AxiosProvider';
+
 import styles from './styles';
 
 function Landing() {
   const { navigate } = useNavigation();
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('connections').then(response => {
+      const { total } = response.data;
+      setTotalConnections(total);
+    })
+  }, []);
 
   function handleNavigateToGiveClassesPage() {
     navigate('Teach');
@@ -50,7 +60,7 @@ function Landing() {
       </View>
 
       <Text style={styles.totalConnections}>
-        Total de 285 conexões já realizadas {' '}
+        Total de {totalConnections} conexões já realizadas {' '}
         <Image source={heartIcon} />
       </Text>
 
